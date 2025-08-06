7d87e41aab2795a6333ba909ea8050b65f309d45
Rops12 <matheus.oliveira.correia12@gmail.com>
2025-08-06 16:03:35 -0300

    v1.5

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,8 +1,3 @@
 /// <reference types="react-scripts" />
 
-// Adicione o código abaixo para corrigir o problema com react-icons
-declare module 'react-icons/fi' {
-  import { IconType } from 'react-icons';
-  const icon: IconType;
-  export = icon;
-}
+// Isso estava INCORRETO para este caso
